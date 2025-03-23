849baadc6d56f827ce9219ef2bdd29dffca1ec84
ArielGolanski <ariariel12@gmail.com>
2025-03-23 22:36:22 +0200

    23/03/25

--- MaxDepthOfBinTree/Solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-
-        if not root:
-            return 0
-        
-        return max((1 + self.maxDepth(root.left)), (1 + self.maxDepth(root.right)))
--- ValidPalindrome/Solution.py
+++ ValidPalindrome/Solution.py
@@ -1,43 +1,20 @@
-# Difficulty level: easy
-
-# Given a string s, return true if it is a palindrome, otherwise return false.
-
-#A palindrome is a string that reads the same forward and backward.
-#It is also case-insensitive and ignores all non-alphanumeric characters.
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         i = 0
         j = len(s) - 1
-    
+
+        s = s.lower()
+
         while i < j:
-     
-            while i < j and not s[i].isalnum():
-                i += 1
-        
-            while i < j and not s[j].isalnum():
-                j -= 1
-        
-        
-            if s[i].lower() != s[j].lower():
+            while i < j and not (s[i].isalnum()):
+                i+=1
+            while i < j and not (s[j].isalnum()):
+                j-=1
+            if s[i] != s[j]:
                 return False
+            else:
+                i+=1
+                j-=1
         
-            i += 1
-            j -= 1
-    
-        return True              
         
-
-def main():
-    # Create an instance of Solution
-    sol = Solution()
-
-    # Example 1: Removing elements
-    s = "Was it a car or a cat I saw?"
-    print(f"Original sentence: {s}")
-    result = sol.isPalindrome(s)
-    print(f"{result}\n")
-
-
-if __name__ == "__main__":
-    main()
+        return True
