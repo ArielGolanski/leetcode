fdbdb093df64227392d0b1dd355c48e2eba6db1f
ArielGolanski <ariariel12@gmail.com>
2025-03-26 20:53:50 +0200

    26/03/2025

---
+++ ReverseLinkedLst/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        l = head
+        stack = []
+        if l == None:
+            return None
+        elif l.next == None:
+            return l
+        else:
+            r = l.next
+            while r.next != None:
+                temp = r.next
+                r.next = l
+                l = r
+                r = temp
+            r.next = l
+            head.next = None
+            head = r
+        return head
+                
