dd851f048b3fb7e5fdf7de310fa9c40b0b71ed52
ArielGolanski <ariariel12@gmail.com>
2025-04-30 08:23:24 +0300

    30/04/2025

---
+++ MinimumStack/Solution.py
@@ -0,0 +1,19 @@
+class MinStack:
+
+    def __init__(self):
+        self.min_stack = []
+        self.not_min_stack = []
+        self.top_stack = []
+
+    def push(self, val: int) -> None:
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
